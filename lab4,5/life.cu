41b2b04f381424a89400e2bf50ff898d7d086e6b
Fabrício Ceolin <fabceolin@gmail.com>
2014-11-09 10:31:16 -0200

    lab 5 ex1-a

---
+++ lab4,5/life.cu
@@ -0,0 +1,99 @@
+
+#include "utils.h"
+#include <stdlib.h>
+
+#include "life_kernel.cu"
+
+
+int main(int argc, char ** argv)
+{
+    // Definition of parameters
+    int domain_x = 128;	// Multiple of threads_per_block * cells_per_word
+    int domain_y = 128;
+    
+    int cells_per_word = 1;
+    
+    int steps = 2;
+    
+    int threads_per_block = 128;
+    int blocks_x = domain_x / (threads_per_block * cells_per_word);
+    int blocks_y = domain_y;
+    
+    dim3  grid(blocks_x, blocks_y);	// CUDA grid dimensions
+    dim3  threads(threads_per_block);	// CUDA block dimensions
+
+    // Allocation of arrays
+    int * domain_gpu[2] = {NULL, NULL};
+
+	size_t pitch;
+	CUDA_SAFE_CALL(cudaMallocPitch((void**)&domain_gpu[0], &pitch,
+		domain_x / cells_per_word * sizeof(int),
+		domain_y));
+    CUDA_SAFE_CALL(cudaMallocPitch((void**)&domain_gpu[1], &pitch,
+    	domain_x / cells_per_word * sizeof(int),
+		domain_y));
+
+	// Arrays of dimensions pitch * domain.y
+
+	init_kernel<<< grid, threads, 0 >>>(domain_gpu[0], pitch);
+
+    // Timer initialization
+    cudaEvent_t start, stop;
+    CUDA_SAFE_CALL(cudaEventCreate(&start));
+    CUDA_SAFE_CALL(cudaEventCreate(&stop));
+
+    // Start timer
+    CUDA_SAFE_CALL(cudaEventRecord(start, 0));
+
+    // Kernel execution
+    int shared_mem_size = 0;
+    for(int i = 0; i < steps; i++) {
+	    life_kernel<<< grid, threads, shared_mem_size >>>(domain_gpu[i%2],
+	    	domain_gpu[(i+1)%2], domain_x, domain_y, pitch);
+	}
+
+    // Stop timer
+    CUDA_SAFE_CALL(cudaEventRecord(stop, 0));
+    CUDA_SAFE_CALL(cudaEventSynchronize(stop));
+    
+    float elapsedTime;
+    CUDA_SAFE_CALL(cudaEventElapsedTime(&elapsedTime, start, stop));	// In ms
+    printf("GPU time: %f ms\n", elapsedTime);
+
+    CUDA_SAFE_CALL(cudaEventDestroy(start));
+    CUDA_SAFE_CALL(cudaEventDestroy(stop));
+
+    // Get results back
+    int * domain_cpu = (int*)malloc(pitch * domain_y);
+    CUDA_SAFE_CALL(cudaMemcpy(domain_cpu, domain_gpu[steps%2], pitch * domain_y, cudaMemcpyDeviceToHost));
+
+    CUDA_SAFE_CALL(cudaFree(domain_gpu[0]));
+    CUDA_SAFE_CALL(cudaFree(domain_gpu[1]));
+    
+
+    // Count colors
+    int red = 0;
+    int blue = 0;
+    for(int y = 0; y < domain_y; y++)
+    {
+    	for(int x = 0; x < domain_x; x++)
+    	{
+    		int cell = domain_cpu[y * pitch/sizeof(int) + x];
+    		printf("%u", cell);
+    		if(cell == 1) {
+    			red++;
+    		}
+    		else if(cell == 2) {
+    			blue++;
+    		}
+    	}
+    	printf("\n");
+    }
+    
+    printf("Red/Blue cells: %d/%d\n", red, blue);
+    
+    free(domain_cpu);
+    
+    return 0;
+}
+
---
+++ lab4,5/life_kernel.cu
@@ -0,0 +1,90 @@
+
+__global__ void init_kernel(int * domain, int pitch)
+{
+    domain[blockIdx.y * pitch / sizeof(int) + blockIdx.x * blockDim.x + threadIdx.x]
+        = (1664525ul * (blockIdx.x + threadIdx.y + threadIdx.x) + 1013904223ul) % 3;
+}
+
+// Reads a cell at (x+dx, y+dy)
+__device__ int read_cell(int * source_domain, int x, int y, int dx, int dy,
+    int domain_x, int domain_y, int pitch)
+{
+    x = (x + dx) % domain_x;	// Wrap around
+    y = (y + dy) % domain_y;
+    return source_domain[y * (pitch / sizeof(int)) + x];
+}
+
+__device__ void write_cell(int * dest_domain, int x, int y, int dx, int dy,
+    int domain_x, int domain_y, int pitch, int value)
+{
+    x = (x + dx) % domain_x;	// Wrap around
+    y = (y + dy) % domain_y;
+    dest_domain[y * (pitch / sizeof(int)) + x] = value;
+}
+
+// Compute kernel
+__global__ void life_kernel(int * source_domain, int * dest_domain,
+    int domain_x, int domain_y, int pitch)
+{
+    int tx = blockIdx.x * blockDim.x + threadIdx.x;
+    int ty = blockIdx.y;
+    
+    // Read cell
+    int myself = read_cell(source_domain, tx, ty, 0, 0,
+                       domain_x, domain_y, pitch);
+    
+    
+    // TODO: Read the 8 neighbors and count number of blue and red 
+    int blue=0;
+    int red=0;
+    int adjacent_count=0;
+    for (int i=0; i<9;i++) {
+        if (i==4) /* itself */ {
+           continue;
+        }
+        int x = i % 3 - 1;
+        int y = (int) (i / 3) - 1;
+        int near = read_cell(source_domain, tx, ty, x,y,domain_x,domain_y,pitch);
+        switch (near) {
+            case (1):
+                 red++;
+                 break;
+            case (2):
+                 blue++;
+                 break;
+        default:
+           break;
+        }
+        if ( (i+1)%2==0) {
+           if (near>0) {
+               adjacent_count++; 
+           }
+        }
+    }
+
+    int total_near = blue+red;
+    int new_value = myself;
+    
+    // rules
+    if ((total_near)>3) {
+        new_value = 0;
+    }
+
+    if (adjacent_count==1) {
+        new_value = 0;
+    }
+    if ((total_near)==3 && (myself==0)) {
+         if (blue>red) {
+             new_value=2;
+         }
+         else {
+             new_value=1;
+         }
+    }
+    
+
+    write_cell(dest_domain, tx, ty, 0,0,domain_x,domain_y,pitch,new_value);
+    return;
+    
+}
+
